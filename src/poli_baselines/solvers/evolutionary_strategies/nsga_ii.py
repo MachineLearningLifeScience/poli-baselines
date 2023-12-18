e68455688be6024b27ae61cbc69d963ae638dfc8
Miguel González Duque <miguelgondu@gmail.com>
2023-12-18 11:20:14 +0100

    Wraps up documentation for discrete NSGA-ii

--- src/poli_baselines/solvers/evolutionary_strategies/nsga_ii.py
+++ src/poli_baselines/solvers/evolutionary_strategies/nsga_ii.py
@@ -2,10 +2,18 @@
 
 NSGA-II (Non-dominated Sorting Genetic Algorithm II) is a multi-objective
 genetic algorithm that uses a non-dominated sort to rank individuals and
-a crowding distance to maintain diversity (TODO: fact-check and cite).
+a crowding distance to maintain diversity [1].
 
 We leverage the implementation inside pymoo, and our wrapper for discrete
 problems s.t. they can be used as pymoo Problems.
+
+References:
+-----------
+[1] Deb, K., A. Pratap, S. Agarwal, and T. Meyarivan. “A Fast and Elitist
+    Multiobjective Genetic Algorithm: NSGA-II.” IEEE Transactions on
+    Evolutionary Computation 6, no. 2 (April 2002): 182-97.
+    https://doi.org/10.1109/4235.996017.
+
 """
 from typing import Tuple
 
@@ -33,6 +41,60 @@ from poli_baselines.core.utils.pymoo import (
 
 
 class Discrete_NSGA_II(AbstractSolver):
+    """Discrete NSGA-II solver for multi-objective optimization problems.
+
+    NSGA-II is a multi-objective genetic algorithm that uses a non-dominated
+    sort to rank individuals and a crowding distance to maintain diversity [1].
+
+    This discrete NSGA-II solver uses the implementation of NSGA-II inside
+    pymoo, and our wrapper for discrete problems s.t. they can be used as
+    pymoo Problems. For those acquainted with pymoo: we use Choice variables
+    inside our definition of the problem. These Choice variables select letters
+    from the alphabet. See the documentation of DiscretePymooProblem for more
+    details.
+
+    Parameters:
+    -----------
+    black_box : AbstractBlackBox
+        The black box function to be optimized.
+    x0 : np.ndarray
+        The initial population of solutions.
+    y0 : np.ndarray
+        The corresponding objective values for the initial population.
+    population_size : int, optional
+        The size of the population, by default 10.
+    initialize_with_x0 : bool, optional
+        Whether to initialize the algorithm with the provided x0, by default True.
+    mutation : Mutation, optional
+        The mutation operator to be used, by default None.
+    sampling : Sampling, optional
+        The sampling operator to be used, by default None.
+    mating : InfillCriterion, optional
+        The mating operator to be used, by default None.
+
+    Attributes:
+    -----------
+    pymoo_problem : DiscretePymooProblem
+        The problem instance for pymoo optimization.
+    algorithm : NSGA2
+        The NSGA-II algorithm instance.
+
+    Methods:
+    --------
+    next_candidate() -> np.ndarray:
+        Get the next candidate solution from the algorithm.
+
+    step() -> Tuple[np.ndarray, np.ndarray]:
+        Perform a single step of the algorithm and return the current population and objective values.
+
+    References:
+    -----------
+    [1] Deb, K., A. Pratap, S. Agarwal, and T. Meyarivan. “A Fast and Elitist
+        Multiobjective Genetic Algorithm: NSGA-II.” IEEE Transactions on
+        Evolutionary Computation 6, no. 2 (April 2002): 182-97.
+        https://doi.org/10.1109/4235.996017.
+    """
+
     def __init__(
         self,
         black_box: AbstractBlackBox,
@@ -44,6 +106,31 @@ class Discrete_NSGA_II(AbstractSolver):
         sampling: Sampling = None,
         mating: InfillCriterion = None,
     ):
+        """
+        Initialize the NSGA_II solver.
+
+        Parameters
+        ----------
+        black_box : AbstractBlackBox
+            The black box function to be optimized.
+        x0 : np.ndarray
+            The initial population of solutions.
+        y0 : np.ndarray
+            The corresponding objective values of the initial population.
+        population_size : int, optional
+            The size of the population, by default 10.
+        initialize_with_x0 : bool, optional
+            Whether to initialize the algorithm with the provided x0, by default True.
+        mutation : Mutation, optional
+            The mutation operator. If None, we use a DiscreteSequenceMutation (see our
+            implementation in the API reference).
+        sampling : Sampling, optional
+            The sampling operator. If None, we use a MixedVariableSampling (see pymoo
+            for more details)
+        mating : InfillCriterion, optional
+            The mating operator. If None, we use a MixedVariableMating with
+            MixedVariableDuplicateElimination (see pymoo for more details).
+        """
         super().__init__(black_box, x0, y0)
 
         assert x0.shape[0] == population_size, (
@@ -89,14 +176,31 @@ class Discrete_NSGA_II(AbstractSolver):
         )
 
     def next_candidate(self) -> np.ndarray:
-        # At this point, the pymoo problem has been initialized with a certain
-        # population.
+        """
+        Generate the next candidate solution using the NSGA-II algorithm.
+
+        Returns:
+        --------
+            x: np.ndarray: The current population of solutions.
+        """
         x = self.algorithm.pop.get("X")
         return x
 
     def step(self) -> Tuple[np.ndarray, np.ndarray]:
         """
-        https://pymoo.org/algorithms/usage.html#Problem-Depdendent
+        Perform a single step of the algorithm, returning population and objective values.
+
+        This implementation uses the ask-tell interface of pymoo, as described in the
+        documentation [1].
+
+        Returns:
+        --------
+            x: np.ndarray: The current population of solutions.
+            y: np.ndarray: The current objective values.
+
+        References:
+        -----------
+        [1] https://pymoo.org/algorithms/usage.html#Problem-Depdendent
         """
         pop = self.algorithm.ask()
 
