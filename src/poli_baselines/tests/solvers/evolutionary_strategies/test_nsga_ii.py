545af5b9ad8095002948e7a08739a39f50a1e0b3
Miguel González Duque <miguelgondu@gmail.com>
2024-01-22 00:52:59 +0100

    Adds tests for random mutations, nsga2 and bo

--- src/poli_baselines/tests/solvers/evolutionary_strategies/test_nsga_ii.py
+++ src/poli_baselines/tests/solvers/evolutionary_strategies/test_nsga_ii.py
@@ -36,5 +36,40 @@ def test_running_nsga_ii():
     solver.solve(max_iter=5, verbose=True)
 
 
-if __name__ == "__main__":
-    test_running_nsga_ii()
+def test_nsga_ii_in_docs():
+    import numpy as np
+
+    from poli.objective_repository import AlohaProblemFactory
+    from poli.core.multi_objective_black_box import MultiObjectiveBlackBox
+
+    from poli_baselines.solvers import DiscreteNSGAII
+
+    population_size = 15
+    batch_size = 10
+    max_iterations = 100
+    num_mutations = 1
+
+    # Creating the aloha problem
+    problem_factory = AlohaProblemFactory()
+    f_aloha, _, _ = problem_factory.create()
+
+    # Putting two copies together to make a multi-objective black box
+    f = MultiObjectiveBlackBox(
+        info=f_aloha.info,
+        objective_functions=[f_aloha, f_aloha],
+    )
+
+    # Creating a random initial population
+    x0 = np.random.choice(f.info.alphabet, size=(batch_size, 5))
+    y0 = f(x0)
+
+    solver = DiscreteNSGAII(
+        black_box=f,
+        x0=x0,
+        y0=y0,
+        population_size=population_size,
+        num_mutations=num_mutations,
+    )
+
+    solver.solve(max_iter=max_iterations)
+    print(f"Best solution: {solver.get_best_solution()}")
--- src/poli_baselines/tests/solvers/simple/test_random_mutation.py
+++ src/poli_baselines/tests/solvers/simple/test_random_mutation.py
@@ -16,3 +16,28 @@ def test_random_mutation_instantiates_and_runs_well():
     )
 
     solver.solve(max_iter=5)
+
+
+def test_random_mutation_in_docs():
+    import numpy as np
+
+    from poli.objective_repository import AlohaProblemFactory
+    from poli_baselines.solvers import RandomMutation
+
+    problem_factory = AlohaProblemFactory()
+
+    f, x0, y0 = problem_factory.create()
+
+    solver = RandomMutation(
+        black_box=f,
+        x0=x0,
+        y0=y0,
+    )
+
+    solver.solve(max_iter=100)
+
+    print(solver.get_best_solution())
+
+
+if __name__ == "__main__":
+    test_random_mutation_in_docs()
