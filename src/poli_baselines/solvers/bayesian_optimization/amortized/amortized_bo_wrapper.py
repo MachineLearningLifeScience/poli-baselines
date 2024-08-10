b2b3cbf15a7b823f4e88da7d2ec8ac3f22a09614
Simon Bartels <sbartels@univ-tlse3.fr>
2024-08-10 15:24:17 +0200

    adapts interface to changes in main and fixes a problem with unaligned sequences

--- src/poli_baselines/solvers/bayesian_optimization/amortized/amortized_bo_wrapper.py
+++ src/poli_baselines/solvers/bayesian_optimization/amortized/amortized_bo_wrapper.py
@@ -1,7 +1,7 @@
 import numpy as np
 from poli.core.abstract_black_box import AbstractBlackBox
 
-from poli_baselines.core.abstract_solver import AbstractSolver
+from poli_baselines.core.step_by_step_solver import StepByStepSolver
 from poli_baselines.solvers.bayesian_optimization.amortized.data import (
     samples_from_arrays,
     Population,
@@ -15,11 +15,15 @@ from poli_baselines.solvers.bayesian_optimization.amortized.domains import (
 )
 
 
-class AmortizedBOWrapper(AbstractSolver):
+class AmortizedBOWrapper(StepByStepSolver):
     def __init__(self, black_box: AbstractBlackBox, x0: np.ndarray, y0: np.ndarray):
         super().__init__(black_box, x0, y0)
+        self.problem_info = black_box.get_black_box_info()
+        alphabet = self.problem_info.get_alphabet()
+        if not self.problem_info.sequences_are_aligned():
+            alphabet = alphabet + [self.problem_info.get_padding_token()]
         self.domain = FixedLengthDiscreteDomain(
-            vocab=Vocabulary(black_box.get_black_box_info().get_alphabet()),
+            vocab=Vocabulary(alphabet),
             length=x0.shape[1],
         )
         self.solver = MutationPredictorSolver(
@@ -32,4 +36,7 @@ class AmortizedBOWrapper(AbstractSolver):
             structures=self.domain.encode(self.x0.tolist()), rewards=self.y0.tolist()
         )
         x = self.solver.propose(num_samples=1, population=Population(samples))
-        return np.array([c for c in self.domain.decode(x)[0]])[np.newaxis, :]
+        s = list(self.domain.decode(x)[0])
+        if not self.problem_info.sequences_are_aligned():
+            s = s + [self.problem_info.get_padding_token()] * (self.problem_info.get_max_sequence_length() - len(s))
+        return np.array([s])
