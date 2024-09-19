a19d3152544f2362dbb07ecde7f45d45de17eef0
Miguel González Duque <miguelgondu@gmail.com>
2024-09-19 14:46:34 +0200

    Implements an example modifying the default hyperparameters

--- examples/06_running_lambo2_on_rasp/run.py
+++ examples/06_running_lambo2_on_rasp/run.py
@@ -13,7 +13,7 @@ sys.path.append(str(THIS_DIR))
 from simple_observer import SimpleObserver, plot_best_y
 
 
-if __name__ == "__main__":
+def run_with_default_hyperparameters():
     RFP_PDBS_DIR = THIS_DIR / "rfp_pdbs"
     ALL_PDBS = list(RFP_PDBS_DIR.rglob("**/*.pdb"))
     problem = RaspProblemFactory().create(
@@ -37,8 +37,6 @@ if __name__ == "__main__":
         black_box=black_box,
         x0=x0,
         y0=y0,
-        config_path="./hydra_configs",
-        config_name="generic_training",
     )
 
     lambo2.solve(max_iter=10)
@@ -50,3 +48,61 @@ if __name__ == "__main__":
     plt.show()
 
     black_box.terminate()
+
+
+def run_with_modified_hyperparameters():
+    """
+    TODO: write docs explaining the original config
+    file from Lambo2.
+    """
+    # Some alternative hyperparameters which
+    # will override what is specified in the Lambo2
+    # configuration. You can find the original config
+    # we use here:
+    # TODO: add link.
+    POPULATION_SIZE = 96
+    MAX_EPOCHS_FOR_PRETRAINING = 4
+
+    RFP_PDBS_DIR = THIS_DIR / "rfp_pdbs"
+    ALL_PDBS = list(RFP_PDBS_DIR.rglob("**/*.pdb"))
+    problem = RaspProblemFactory().create(
+        wildtype_pdb_path=ALL_PDBS,
+        additive=True,
+        chains_to_keep=[p.parent.name.split("_")[1] for p in ALL_PDBS],
+    )
+    black_box = problem.black_box
+
+    observer = SimpleObserver()
+    black_box.set_observer(observer)
+
+    arr = np.load(THIS_DIR / "rasp_seed_data.npz")
+    x0 = arr["x0"]
+    y0 = arr["y0"]
+
+    observer.x_s.append(x0.reshape(-1, 1))
+    observer.y_s.append(y0)
+
+    lambo2 = Lambo2(
+        black_box=black_box,
+        x0=x0,
+        y0=y0,
+        overrides=[
+            f"num_samples={POPULATION_SIZE}",
+            f"max_epochs={MAX_EPOCHS_FOR_PRETRAINING}",
+        ],
+    )
+
+    lambo2.solve(max_iter=4)
+
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    plot_best_y(observer, ax1)
+    plot_best_y(observer, ax2, start_from=x0.shape[0])
+    ax1.axvline(x0.shape[0], color="red")
+    plt.show()
+
+    black_box.terminate()
+
+
+if __name__ == "__main__":
+    # run_with_default_hyperparameters()
+    run_with_modified_hyperparameters()
--- src/poli_baselines/solvers/bayesian_optimization/lambo2/solver.py
+++ src/poli_baselines/solvers/bayesian_optimization/lambo2/solver.py
@@ -1,18 +1,18 @@
 from __future__ import annotations
 from pathlib import Path
-from dataclasses import dataclass
+from uuid import uuid4
 
 import hydra
 import lightning as L
 import numpy as np
 import torch
-from omegaconf import DictConfig, OmegaConf
+from omegaconf import OmegaConf
 from poli.core.abstract_black_box import AbstractBlackBox
 from poli.core.util.seeding import seed_python_numpy_and_torch
 from poli_baselines.core.abstract_solver import AbstractSolver
 
-CACHE_FOLDER = Path.home() / ".cache" / "poli_baselines" / "lambo2"
-CACHE_FOLDER.mkdir(exist_ok=True, parents=True)
+THIS_DIR = Path(__file__).parent.resolve()
+DEFAULT_CONFIG_DIR = THIS_DIR / "hydra_configs"
 
 
 class Lambo2(AbstractSolver):
@@ -21,69 +21,58 @@ class Lambo2(AbstractSolver):
         black_box: AbstractBlackBox,
         x0: np.ndarray | None = None,
         y0: np.ndarray | None = None,
+        config_dir: Path | str | None = None,
+        config_name: str = "generic_training",
         overrides: list[str] | None = None,
         seed: int | None = None,
+        max_epochs_for_retraining: int = 1,
     ):
         super().__init__(black_box=black_box, x0=x0, y0=y0)
-        with hydra.initialize(config_path="./hydra_configs"):
-            cfg = hydra.compose(config_name="generic_training", overrides=overrides)
+        self.experiment_id = f"{uuid4()}"[:8]
+        self.max_epochs_for_retraining = max_epochs_for_retraining
+        if config_dir is None:
+            config_dir = DEFAULT_CONFIG_DIR
+
+        with hydra.initialize_config_dir(config_dir=str(config_dir)):
+            cfg = hydra.compose(config_name=config_name, overrides=overrides)
             OmegaConf.set_struct(cfg, False)
 
-        # set random seed
+        # Setting the random seed
         if seed is not None:
-            cfg.update({"seed": seed})
+            cfg.update({"random_seed": seed})
             seed_python_numpy_and_torch(seed)
-
-        L.seed_everything(seed=cfg.random_seed, workers=True)
-
-        cfg.tasks.protein_generation.protein_seq.data_module.dataset_config = (
-            DictConfig({"_target_": "cortex.data.dataset.NumpyDataset", "train": "???"})
-        )
+            L.seed_everything(seed=cfg.random_seed, workers=True)
         self.cfg = cfg
+
+        # TODO: decide on these criteria
+        if x0 is None:
+            # Run directed evolution or something like that to bootstrap.
+            # TODO: implement.
+            x0 = ...
+        elif x0.shape[0] < cfg.num_samples:
+            original_size = x0.shape[0]
+            # TODO: implement.
+            x0 = ...
+
         tokenized_x0 = np.array([" ".join(x_i) for x_i in x0])
+
+        if y0 is None:
+            y0 = self.black_box(x0)
+        elif y0.shape[0] < x0.shape[0]:
+            y0 = np.vstack([y0, self.black_box(x0[original_size:])])
+
         self.history_for_training = {
             "x": [tokenized_x0],
             "y": [y0.flatten()],
         }
 
-        # TODO: add an ID for the run.
-        self.model_path = CACHE_FOLDER / "ongoing_model.ckpt"
-        model = self.train_model_with_history(
+        # Pre-training the model.
+        MODEL_FOLDER = Path(cfg.data_dir) / self.experiment_id
+        MODEL_FOLDER.mkdir(exist_ok=True, parents=True)
+        self.model_path = MODEL_FOLDER / "ongoing_model.ckpt"
+        self.train_model_with_history(
             save_checkpoint_to=self.model_path,
             max_epochs=cfg.max_epochs,
-        )
-
-        if x0 is not None:
-            sorted_y0_idxs = np.argsort(y0.flatten())[::-1]
-            candidate_points = tokenized_x0[sorted_y0_idxs[:16]]
-        else:
-            # TODO:
-            # What should we do here? Just propose some random
-            # sequences?
-            candidate_points = ...
-
-        acq_fn_runtime_kwargs = hydra.utils.call(
-            cfg.guidance_objective.runtime_kwargs,
-            model=model,
-            candidate_points=candidate_points,
-        )
-        acq_fn = hydra.utils.instantiate(
-            cfg.guidance_objective.static_kwargs, **acq_fn_runtime_kwargs
-        )
-
-        tokenizer_transform = model.root_nodes["protein_seq"].eval_transform
-        tokenizer = tokenizer_transform[0].tokenizer
-        tok_idxs = tokenizer_transform(candidate_points)
-        is_mutable = tokenizer.get_corruptible_mask(tok_idxs)
-        tok_idxs = tokenizer_transform(candidate_points)
-
-        self.optimizer = hydra.utils.instantiate(
-            cfg.optim,
-            params=tok_idxs,
-            is_mutable=is_mutable,
-            model=model,
-            objective=acq_fn,
-            constraint_fn=None,
         )
 
     def train_model_with_history(
@@ -92,13 +81,17 @@ class Lambo2(AbstractSolver):
         save_checkpoint_to: Path | None = None,
         max_epochs: int = 2,
     ) -> L.LightningModule:
+        # TODO: what to do on empty histories? Should
+        # we just skip?
         model = hydra.utils.instantiate(self.cfg.tree)
         model.build_tree(self.cfg, skip_task_setup=True)
 
-        # TODO: add loading the previous weights
         if load_checkpoint_from is not None and load_checkpoint_from.exists():
             model.load_state_dict(
-                torch.load(load_checkpoint_from, map_location="cpu")["state_dict"]
+                torch.load(
+                    load_checkpoint_from,
+                    map_location="cpu",
+                )["state_dict"]
             )
 
         x = np.concatenate(self.history_for_training["x"])
@@ -130,8 +123,9 @@ class Lambo2(AbstractSolver):
             )
 
         # instantiate trainer, set logger
-        self.cfg.update({"max_epochs": max_epochs})
-        self.trainer: L.Trainer = hydra.utils.instantiate(self.cfg.trainer)
+        self.trainer: L.Trainer = hydra.utils.instantiate(
+            self.cfg.trainer, max_epochs=max_epochs
+        )
         self.trainer.fit(
             model,
             train_dataloaders=model.get_dataloader(split="train"),
@@ -143,62 +137,72 @@ class Lambo2(AbstractSolver):
 
         return model
 
-    def solve(
-        self,
-        max_iter: int = 100,
-        n_initial_points: int = 0,
-        seed: int | None = None,
-    ) -> None:
+    def get_candidate_points_from_history(self) -> np.ndarray:
+        x = np.concatenate(self.history_for_training["x"], axis=0)
+        y = np.concatenate(self.history_for_training["y"], axis=0)
+        sorted_y0_idxs = np.argsort(y.flatten())[::-1]
+        candidate_points = x[sorted_y0_idxs[: self.cfg.num_samples]]
+
+        return candidate_points
+
+    def step(self):
+        """
+        Loads the model, runs the optimizer (LaMBO2) for the
+        number of steps in the config, computes new proposal,
+        evaluates on the black box and updates history.
+        """
+        # Load the model and optimizer
+        model = self.train_model_with_history(
+            load_checkpoint_from=self.model_path,
+            max_epochs=self.max_epochs_for_retraining,
+        )
+
+        # Builds the acquisition function
+        candidate_points = self.get_candidate_points_from_history()
+        acq_fn_runtime_kwargs = hydra.utils.call(
+            self.cfg.guidance_objective.runtime_kwargs,
+            model=model,
+            candidate_points=candidate_points,
+        )
+        acq_fn = hydra.utils.instantiate(
+            self.cfg.guidance_objective.static_kwargs, **acq_fn_runtime_kwargs
+        )
+
+        # Builds the optimizer
+        tokenizer_transform = model.root_nodes["protein_seq"].eval_transform
+        tokenizer = tokenizer_transform[0].tokenizer
+        tok_idxs = tokenizer_transform(candidate_points)
+        is_mutable = tokenizer.get_corruptible_mask(tok_idxs)
+        tok_idxs = tokenizer_transform(candidate_points)
+        optimizer = hydra.utils.instantiate(
+            self.cfg.optim,
+            params=tok_idxs,
+            is_mutable=is_mutable,
+            model=model,
+            objective=acq_fn,
+            constraint_fn=None,
+        )
+
+        # Compute proposals using the optimizer
+        for _ in range(self.cfg.num_steps):
+            # Take a step on the optimizer, diffusing towards promising sequences.
+            optimizer.step()
+
+        # Get the most promising sequences from the optimizer
+        best_solutions = optimizer.get_best_solutions()
+        new_designs = best_solutions["protein_seq"].values
+        new_designs_for_black_box = np.array(
+            [seq.replace(" ", "") for seq in new_designs]
+        )
+
+        # Evaluate the black box
+        new_y = self.black_box(new_designs_for_black_box)
+        print(new_y)
+
+        # Updating the history that is used for training.
+        self.history_for_training["x"].append(new_designs)
+        self.history_for_training["y"].append(new_y.flatten())
+
+    def solve(self, max_iter: int = 100) -> None:
         for _ in range(max_iter):
-            # Load the model and optimizer
-            model = self.train_model_with_history(
-                load_checkpoint_from=self.model_path, max_epochs=2
-            )
-            x = np.concatenate(self.history_for_training["x"], axis=0)
-            y = np.concatenate(self.history_for_training["y"], axis=0)
-
-            sorted_y0_idxs = np.argsort(y.flatten())[::-1]
-            candidate_points = x[sorted_y0_idxs[:16]]
-
-            acq_fn_runtime_kwargs = hydra.utils.call(
-                self.cfg.guidance_objective.runtime_kwargs,
-                model=model,
-                candidate_points=candidate_points,
-            )
-            acq_fn = hydra.utils.instantiate(
-                self.cfg.guidance_objective.static_kwargs, **acq_fn_runtime_kwargs
-            )
-
-            tokenizer_transform = model.root_nodes["protein_seq"].eval_transform
-            tokenizer = tokenizer_transform[0].tokenizer
-            tok_idxs = tokenizer_transform(candidate_points)
-            is_mutable = tokenizer.get_corruptible_mask(tok_idxs)
-            tok_idxs = tokenizer_transform(candidate_points)
-
-            optimizer = hydra.utils.instantiate(
-                self.cfg.optim,
-                params=tok_idxs,
-                is_mutable=is_mutable,
-                model=model,
-                objective=acq_fn,
-                constraint_fn=None,
-            )
-
-            for _ in range(self.cfg.num_steps):
-                # Take a step on the optimizer, diffusing towards promising sequences.
-                optimizer.step()
-
-            # Get the most promising sequences from the optimizer
-            best_solutions = optimizer.get_best_solutions()
-            new_designs = best_solutions["protein_seq"].values
-            new_designs_for_black_box = np.array(
-                [seq.replace(" ", "") for seq in new_designs]
-            )
-
-            # Evaluate the black box
-            new_y = self.black_box(new_designs_for_black_box)
-            print(new_y)
-
-            # Retraining the model
-            self.history_for_training["x"].append(new_designs)
-            self.history_for_training["y"].append(new_y.flatten())
+            self.step()
