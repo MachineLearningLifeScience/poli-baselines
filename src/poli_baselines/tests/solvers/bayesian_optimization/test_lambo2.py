d77fcd3e44bc582b0ccf26286be0bb0fcfa3dd80
Miguel González Duque <miguelgondu@gmail.com>
2024-09-23 13:20:05 +0200

    Simplifies test by mocking a function call

--- src/poli_baselines/tests/solvers/bayesian_optimization/test_lambo2.py
+++ src/poli_baselines/tests/solvers/bayesian_optimization/test_lambo2.py
@@ -1,9 +1,12 @@
 import importlib
 from pathlib import Path
+from unittest.mock import patch
 
 import pytest
 
-from poli.repository import RaspProblemFactory
+import numpy as np
+
+from poli.repository import AlohaProblemFactory
 
 if importlib.util.find_spec("cortex") is None:
     pytest.skip("Cortex is not installed.", allow_module_level=True)
@@ -11,20 +14,17 @@ if importlib.util.find_spec("cortex") is None:
 TEST_ASSETS = Path(__file__).parent.parent.parent / "test_files"
 
 
-def test_lambo2_runs_on_rasp():
+def test_lambo2_runs_on_mocked_function():
     from poli_baselines.solvers.bayesian_optimization.lambo2 import LaMBO2
 
-    problem = RaspProblemFactory().create(
-        wildtype_pdb_path=TEST_ASSETS / "2vad_A.pdb",
-        chains_to_keep=["A"],
-        additive=True,
-    )
+    problem = AlohaProblemFactory().create()
     f, x0 = problem.black_box, problem.x0
 
-    solver = LaMBO2(
-        black_box=f,
-        x0=x0,
-        y0=f(x0),
-    )
+    with patch.object(f, "__call__", new=lambda x: np.zeros((x.shape[0], 1))) as _:
+        solver = LaMBO2(
+            black_box=f,
+            x0=x0,
+            y0=f(x0),
+        )
 
-    solver.solve(max_iter=1)
+        solver.solve(max_iter=1)
